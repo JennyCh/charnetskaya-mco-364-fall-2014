98965f2bc8247a03b645941a44f98415ef19ef41
JennyCh <ycharnetskaya@gmail.com>
2014-09-22 21:09:55 -0400

    now multithreaded!!!
    Added multithreading, to have a new thread for every request. As you
    refresh the page it shows the number of the request that server
    recieved.
    PS. must be tested with Firefox browser (Chrome/IE do not cache their
    requests and the number of requests gets totally messed up)

--- src/charnetskaya/network/ResponseServer.java
+++ src/charnetskaya/network/ResponseServer.java
@@ -1,6 +1,8 @@
 package charnetskaya.network;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,29 +14,41 @@ public class ResponseServer {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int counter = 0;
-		ServerSocket serverSocket = new ServerSocket(8080);
-
+		int clientNum = 0;
+		ServerSocket serverSocket = new ServerSocket(8000);
+		Socket socket = null;
 		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream in = socket.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-			String line;
-			while (!("".equals(line = reader.readLine()))) {
-				System.out.println(line);
-			}
-
-			OutputStream out = socket.getOutputStream();
-			String response = "<h2>"+ counter + "</h2>";
-			out.write("HTTP/1.1 200 OK\n".getBytes());
-			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
-			out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
-			out.write(response.getBytes());
-			out.flush();
-			out.close();
-			counter++;
+			socket = serverSocket.accept();
+			new HandleClient(socket, clientNum++).run();
 		}
+	}
+
+	static class HandleClient implements Runnable {
+		Socket socket;
+		int counter;
+
+		public HandleClient(Socket socket, int counter) {
+			this.socket = socket;
+			this.counter = counter;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {				
+				OutputStream out = socket.getOutputStream();
+				String response = "<h2>" + counter + "</h2>";
+				out.write("HTTP/1.1 200 OK\n".getBytes());
+				out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
+				out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
+				out.write(response.getBytes());
+				out.flush();
+				out.close();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+		}
+
 	}
 
 }
